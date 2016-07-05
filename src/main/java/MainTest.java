d042d1b0440f481ea4b19f048641e97f1827decb
ichernenko <ichernenko2@mail.ru>
2016-07-05 14:09:05 +0300

    Сделан небольшой рефакторинг, добавлена документация

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -2,6 +2,5 @@
 public class MainTest {
     public static void main(String... args) {
         PhraseHandler ph = new PhraseHandler("быть");
-        ph.printFoundVerb();
     }
 }
--- src/main/java/PhraseHandler.java
+++ src/main/java/PhraseHandler.java
@@ -1,85 +1,54 @@
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class PhraseHandler {
-    public static Connection conn;
-    public static Statement statmt;
-    public static ResultSet resSet;
+class PhraseHandler {
     private String phrase = null;
-    static String fileName = "src\\main\\resources\\mainDictionary.dic";
-
 
     PhraseHandler(String phrase) {
         this.phrase = phrase;
         getAllVerbs();
     }
 
-    public void printFoundVerb() {
-        try {
-            Files.lines(Paths.get(fileName), StandardCharsets.UTF_8)
-                    .filter(a -> a.equals(phrase))
-                    .forEach(System.out::println);
-        } catch (Exception e) {
-            System.out.println("TTTTT");
+    private void getAllVerbs() {
+        if(loadDBDriver()) {
+            if(loadAllVerbs()) {
+                System.out.println("All verbs are loaded!");
+            }
         }
     }
 
-    private void getAllVerbs() {
-        openDB();
-        readDB();
-        closeDB();
-
-    }
-
-    private static void openDB() {
-        conn = null;
+    private boolean loadDBDriver() {
+        boolean sign = false;
         try {
             Class.forName("org.sqlite.JDBC");
-            conn = DriverManager.getConnection("jdbc:sqlite:db\\solverDB.s3db");
+            sign = true;
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("DB driver wasn't loaded!");
         }
-
-        System.out.println("База Подключена!");
+        return sign;
     }
 
-    public static void readDB() {
-        try {
-            statmt = conn.createStatement();
-            resSet = statmt.executeQuery("SELECT * FROM verbs");
+    private boolean loadAllVerbs() {
+        boolean sign = false;
+        try (Connection con = DriverManager.getConnection("jdbc:sqlite:db\\solverDB.s3db");
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery("SELECT * FROM verbs")) {
 
-            while (resSet.next()) {
-                int id = resSet.getInt("id");
-                String name = resSet.getString("verb");
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("verb");
                 System.out.println("ID = " + id);
                 System.out.println("name = " + name);
                 System.out.println();
             }
-
+            sign = true;
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Verbs weren't loaded!");
         }
 
-        System.out.println("Таблица выведена");
-    }
-
-    public static void closeDB() {
-        try {
-            resSet.close();
-            statmt.close();
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("DB is closed");
+        return sign;
     }
 }
